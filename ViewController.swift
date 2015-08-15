42a7bc1cd5ff268ef1049b26d2bd70a96470f844
gdsanders <gd@digitalequity.com>
2015-08-15 14:02:57 -0500

    removeStar func

--- ViewController.swift
+++ ViewController.swift
@@ -28,10 +28,24 @@ class ViewController: UIViewController
     
     @IBAction func addStar(sender: AnyObject) {
         let starImgVw:UIImageView = UIImageView(image: UIImage(named: "star"))
+        starImgVw.contentMode = .ScaleAspectFit
         self.horizontalStackView.addArrangedSubview(starImgVw)
         UIView.animateWithDuration(0.25, animations: {
             self.horizontalStackView.layoutIfNeeded()
         })
+
+    }
     
+    
+    @IBAction func removeStar(sender: AnyObject) {
+        let star:UIView? = self.horizontalStackView.arrangedSubviews.last
+        if let aStar = star
+        {
+            self.horizontalStackView.removeArrangedSubview(aStar)
+            aStar.removeFromSuperview()
+            UIView.animateWithDuration(0.25, animations: {
+                self.horizontalStackView.layoutIfNeeded()
+            })
+        }
     }
 }
